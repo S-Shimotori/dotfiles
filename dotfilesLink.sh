981ac962511e8686e9e34273f2702626c263cf8c
S-Shimotori <komata.h.aa@m.titech.ac.jp>
2015-11-01 00:40:16 +0900

    Modify file path for neovim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/init/*.vim ~/.vim/userautoload/init/
-ln -sf ~/dotfiles/plugins/*.vim ~/.vim/userautoload/plugins/
-ln -sf ~/dotfiles/.nvimrc ~/.nvimrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/init/*.vim ~/.vim/userautoload/init/
+ln -sf ~/dotfiles/vim/plugins/*.vim ~/.vim/userautoload/plugins/
+ln -sf ~/dotfiles/neovim/init.vim ~/.config/nvim/
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
